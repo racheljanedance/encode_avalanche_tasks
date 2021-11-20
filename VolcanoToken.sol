5fd1b31cf8f0e418f2d3ada9d09cb00c66317e65
racheljanedance <racheljanedance@googlemail.com>
2021-11-20 22:32:31 +0000

    volcano token added

---
+++ VolcanoToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"; 
+
+contract volcanoToken {
+    
+    
+}
