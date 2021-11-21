8f120aea19133a8935bb5b777e453792f1dc666e
racheljanedance <racheljanedance@googlemail.com>
2021-11-21 17:56:21 +0000

    compiled and deployed

--- VolcanoToken.sol
+++ VolcanoToken.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"; 
 
-contract volcanoToken {
+contract volcanoToken is Ownable, ERC721("TOKEN", "TKN") {
     
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+    
+    // 
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_; 
+        _symbol = symbol_; 
+    }
     
 }
